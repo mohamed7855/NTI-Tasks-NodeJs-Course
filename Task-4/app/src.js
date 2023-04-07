f975dbf836a943893681ee42d77d6c88ae3e56a4
Mohamed Omara <martm7726@gmail.com>
2023-04-07 21:25:41 +0200

    Task4 copyOf Task3 and will edit

---
+++ Task-4/app/src.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const path = require("path");
+
+const staticDir = path.join(__dirname, "../resources/public");
+const viewsDir = path.join(__dirname, "../resources/views");
+const partialsDir = path.join(__dirname, "../resources/layout");
+
+app.use(express.static(staticDir));
+app.set("view engine", "hbs");
+app.set("views", viewsDir);
+hbs.registerPartials(partialsDir);
+
+// to Use method post (req.body)
+app.use(express.urlencoded({ extended: true }));
+
+const taskRoutes = require("./routes/task.routes");
+app.use(taskRoutes);
+app.use("*", (req, res) => {
+  res.render("error404", { pageTitle: "ErrorPage" });
+});
+module.exports = app;
