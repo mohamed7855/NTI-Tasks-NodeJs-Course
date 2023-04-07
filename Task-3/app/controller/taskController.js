4940337ec2d268a03e62918b1472b572ba6ed7a6
Mohamed Omara <martm7726@gmail.com>
2023-04-07 04:27:41 +0200

    Task3 activate-done & search-static-data..!

--- Task-3/app/controller/taskController.js
+++ Task-3/app/controller/taskController.js
@@ -15,18 +15,18 @@ class Task {
   };
 
   static addLogic = (req, res) => {
-    const newTask = { id: Date.now(), ...req.query };
+    const newTask = { id: Date.now(), status: false, ...req.query };
     let allTasks = deal.readJsonData(fileName);
     allTasks.push(newTask);
     deal.writeJsonData(fileName, allTasks);
     res.redirect("/");
   };
 
-  static single = (req, res) => {
+  static show = (req, res) => {
     const Task = deal
       .readJsonData(fileName)
       .find((task) => task.id == req.params.id);
-    res.render("single", { pageTitle: "singleTask", Task });
+    res.render("show", { pageTitle: "showTask", Task });
   };
 
   static deleteAll = (req, res) => {
@@ -46,12 +46,39 @@ class Task {
     const allTasks = deal.readJsonData(fileName);
     const id = req.params.id;
     const task = allTasks.find((ele) => ele.id == id);
-    // res.send(task);
     res.render("edit", { pageTitle: "editTask", task });
   };
 
   static editLogic = (req, res) => {
+    const id = req.params.id;
+    const allTasks = deal.readJsonData(fileName);
+    const index = allTasks.findIndex((u) => u.id == id);
+    allTasks[index] = { id, ...req.query };
+    deal.writeJsonData(fileName, allTasks);
+    res.redirect(`/show/${id}`);
+  };
+  static activate = (req, res) => {
+    const allTasks = deal.readJsonData(fileName);
+    const id = req.params.id;
+    const index = allTasks.findIndex((ele) => ele.id == id);
+    if ((allTasks[index].status = true)) deal.writeJsonData(fileName, allTasks);
+    else {
+    }
     res.redirect("/");
+  };
+
+  static search = (req, res) => {
+    let allTasks = deal.readJsonData(fileName);
+    let data = "h";
+    // let data = document.getElementById("myInput").value;
+    allTasks = allTasks.filter(
+      (ele) => ele.title.includes(data) || ele.content.includes(data)
+    );
+    res.render("all", {
+      pageTitle: "allTasks",
+      allTasks,
+      hasData: allTasks.length,
+    });
   };
 }
 
--- Task-3/app/routes/task.routes.js
+++ Task-3/app/routes/task.routes.js
@@ -3,10 +3,12 @@ const router = require("express").Router();
 router.get("/", TaskController.all);
 router.get("/add", TaskController.add);
 router.get("/addLogic", TaskController.addLogic);
-router.get("/single/:id", TaskController.single);
+router.get("/show/:id", TaskController.show);
 router.get("/deleteAll", TaskController.deleteAll);
 router.get("/delete/:id", TaskController.delete);
 router.get("/edit/:id", TaskController.edit);
-router.get("/editLogic", TaskController.editLogic);
+router.get("/editLogic/:id", TaskController.editLogic);
+router.get("/activate/:id", TaskController.activate);
+router.get("/search", TaskController.search);
 
 module.exports = router;
